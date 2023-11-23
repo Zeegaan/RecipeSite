a148416468f54fbc4ce433f918a67776ad08e392
Zeegaan <nge@umbraco.dk>
2023-11-23 12:48:28 +0100

    Add new property editor

---
+++ UmbracoProj/AiController.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace CycleHack;
+
+public class AiController : UmbracoApiController
+{
+    public IActionResult GetRecipe(AiText input)
+    {
+        string myString = input.Text;
+        return Ok();
+    }
+}
+
+[DataContract]
+public class AiText
+{
+    [DataMember(Name = "text")]
+    public string Text { get; set; }
+}
+
+
---
+++ UmbracoProj/App_Plugins/Suggestions/suggestion.controller.js
@@ -0,0 +1,11 @@
+﻿angular.module("umbraco")    .controller("SuggestionPluginController",
+// Scope object is the main object which is used to pass information from the controller to the view.
+    function ($scope, $http, umbRequestHelper) {
+        // The controller assigns the behavior to scope as defined by the getSuggestion method, which is invoked when the user clicks on the 'Give me Suggestions!' button.
+        $scope.getSuggestion = function () {
+            return umbRequestHelper.resourcePromise(
+                $http.post("https://localhost:44351/umbraco/api/ai/GetRecipe", {text : $scope.model.value}),
+                "Failed to get recipe"
+            );
+        }
+    });
